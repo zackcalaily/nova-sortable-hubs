f530a5ae31199bc58a21e4f34920e6aa9645a22f
Zack Calaily <zack.calaily@gmail.com>
2019-07-02 22:11:57 -0400

    Updates

---
+++ precache-manifest.fb5dc3d8d9a6c1ccaee47877ad7bb4e1.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a7e9b5a6677d6f21e83cb220ba05a9",
+    "url": "/nova-sortable-hubs/index.html"
+  },
+  {
+    "revision": "2966221aab6a39843c68",
+    "url": "/nova-sortable-hubs/static/css/main.eabf2512.chunk.css"
+  },
+  {
+    "revision": "0c9977743ae71097dc41",
+    "url": "/nova-sortable-hubs/static/js/2.b25ade84.chunk.js"
+  },
+  {
+    "revision": "2966221aab6a39843c68",
+    "url": "/nova-sortable-hubs/static/js/main.f1720427.chunk.js"
+  },
+  {
+    "revision": "febdcc5166ef2ce4a0c2",
+    "url": "/nova-sortable-hubs/static/js/runtime~main.8596a27a.js"
+  },
+  {
+    "revision": "099ae3f20a17e18ba3b140a7c119b4b5",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Bold.099ae3f2.woff"
+  },
+  {
+    "revision": "d1ee1a8c0010a5e6f558222370d52477",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Bold.d1ee1a8c.woff2"
+  },
+  {
+    "revision": "d7b4796dd8a99fe425411a5c02745a82",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Bold.d7b4796d.svg"
+  },
+  {
+    "revision": "e8a4a016f51fb5d2c101c2853fd0ec89",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Bold.e8a4a016.eot"
+  },
+  {
+    "revision": "f0d6b4ec294fdf0c05d14c21467dc0e3",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Bold.f0d6b4ec.ttf"
+  },
+  {
+    "revision": "41d7590878a2dcacf5c87df3ecac3a51",
+    "url": "/nova-sortable-hubs/static/media/TedNext-BoldItalic.41d75908.woff"
+  },
+  {
+    "revision": "460037a66384d2f491548ae909f90029",
+    "url": "/nova-sortable-hubs/static/media/TedNext-BoldItalic.460037a6.svg"
+  },
+  {
+    "revision": "9192ad6cbfcba373df1545d303c14cc3",
+    "url": "/nova-sortable-hubs/static/media/TedNext-BoldItalic.9192ad6c.eot"
+  },
+  {
+    "revision": "94e52605754e29c1a4a9525164926c9d",
+    "url": "/nova-sortable-hubs/static/media/TedNext-BoldItalic.94e52605.ttf"
+  },
+  {
+    "revision": "e838cf29a5c80a8fc2e5e85b7cf612c6",
+    "url": "/nova-sortable-hubs/static/media/TedNext-BoldItalic.e838cf29.woff2"
+  },
+  {
+    "revision": "25bb3c1e4a697cedf0a37ca8edda80a4",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Italic.25bb3c1e.woff2"
+  },
+  {
+    "revision": "4aaaf425f14fbabcbc7566d8b307b64e",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Italic.4aaaf425.eot"
+  },
+  {
+    "revision": "6dd7e4efbdadc1731ac926ad7c3e0bdd",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Italic.6dd7e4ef.svg"
+  },
+  {
+    "revision": "960416ba6a75dbc77ca761cc6b1e4fe9",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Italic.960416ba.ttf"
+  },
+  {
+    "revision": "b423f118c4487aa48b2665de62e51dc8",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Italic.b423f118.woff"
+  },
+  {
+    "revision": "01fab3c91a697c3e3db5e743fb0c99c0",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Light.01fab3c9.woff2"
+  },
+  {
+    "revision": "0ad2c411cb584e7e34fe6a0d4bfada9b",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Light.0ad2c411.ttf"
+  },
+  {
+    "revision": "9b55f1870773358916d434e36712cca3",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Light.9b55f187.eot"
+  },
+  {
+    "revision": "ec1ee587bae62ba3239e884124aa92d8",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Light.ec1ee587.svg"
+  },
+  {
+    "revision": "fe560099928cc28d2b2c58d9f2fec976",
+    "url": "/nova-sortable-hubs/static/media/TedNext-Light.fe560099.woff"
+  },
+  {
+    "revision": "726971766f9b06f5c7fb3c519f957431",
+    "url": "/nova-sortable-hubs/static/media/TedNext-LightItalic.72697176.svg"
+  },
+  {
+    "revision": "c55f1cf6264b4da9d42f9779ea57093e",
+    "url": "/nova-sortable-hubs/static/media/TedNext-LightItalic.c55f1cf6.woff"
+  },
+  {
+    "revision": "c761fe8a5e60867f78ddb1b8da3b91ba",
+    "url": "/nova-sortable-hubs/static/media/TedNext-LightItalic.c761fe8a.eot"
+  },
+  {
+    "revision": "d8800bedcbf9d351e1d95e026d07d130",
+    "url": "/nova-sortable-hubs/static/media/TedNext-LightItalic.d8800bed.ttf"
+  },
+  {
+    "revision": "ea60970fb8db7d7d608ec1b96254ccba",
+    "url": "/nova-sortable-hubs/static/media/TedNext-LightItalic.ea60970f.woff2"
+  },
+  {
+    "revision": "3e933131b46322e75574df2d5267ef7c",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBold.3e933131.eot"
+  },
+  {
+    "revision": "75ea8bd8723fdeded3ec1357dbdec6e4",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBold.75ea8bd8.woff"
+  },
+  {
+    "revision": "9fcd046743946dd7fe49373252a15472",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBold.9fcd0467.woff2"
+  },
+  {
+    "revision": "fffdc4a9460f95f1462d290374509a2a",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBold.fffdc4a9.svg"
+  },
+  {
+    "revision": "0e22f078fdc69b76ee96a16d9c690ea7",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBoldItalic.0e22f078.woff"
+  },
+  {
+    "revision": "3792eda8282f11ab9b25f345568e4c99",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBoldItalic.3792eda8.eot"
+  },
+  {
+    "revision": "81ff81f7de62d5f61b171142aed8d9bb",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBoldItalic.81ff81f7.svg"
+  },
+  {
+    "revision": "c8fe5b55ac5b1a6aec75baf6c9cc99d9",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBoldItalic.c8fe5b55.woff2"
+  },
+  {
+    "revision": "eafbf3609b0fdf9b8f624a63e3993142",
+    "url": "/nova-sortable-hubs/static/media/TedNext-SemiBoldItalic.eafbf360.ttf"
+  },
+  {
+    "revision": "15ba45cd9cc8965e1752d9260e3560c8",
+    "url": "/nova-sortable-hubs/static/media/TedNext.15ba45cd.eot"
+  },
+  {
+    "revision": "607596341043e551a88015a6d620d7d7",
+    "url": "/nova-sortable-hubs/static/media/TedNext.60759634.svg"
+  },
+  {
+    "revision": "7d480456ceb774b282287f7c4bd106cf",
+    "url": "/nova-sortable-hubs/static/media/TedNext.7d480456.ttf"
+  },
+  {
+    "revision": "83b5527b310e43b571ed876a714667c0",
+    "url": "/nova-sortable-hubs/static/media/TedNext.83b5527b.woff"
+  },
+  {
+    "revision": "b846c33a1486c09bc8f9c7ab69da80b2",
+    "url": "/nova-sortable-hubs/static/media/TedNext.b846c33a.woff2"
+  },
+  {
+    "revision": "52e520e01cbeaf56e78980e8ce073909",
+    "url": "/nova-sortable-hubs/static/media/mobius.52e520e0.svg"
+  }
+]);
